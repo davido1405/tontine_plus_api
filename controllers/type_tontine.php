46bb262972c8a4eaea9ed1f21154c39d7a253cef
David Tizie <tiziedavid88@gmail.com>
2025-09-13 17:47:31 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   controllers/fcm_debug.log
    	modified:   controllers/notifications.php
    	modified:   controllers/type_tontine.php

--- controllers/type_tontine.php
+++ controllers/type_tontine.php
@@ -2,7 +2,7 @@
 include_once __DIR__ . '/../config/db.php';
 
 include_once __DIR__ . '/../helpers/responses.php';
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 use Firebase\JWT\JWT;
 
 function lister_type_tontine(){
