3ab8a12ad24ecbf493eb57e2480eeff4ba993336
David Tizie <tiziedavid88@gmail.com>
2025-09-30 21:09:01 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     30/09/2025 à 21:10
     Changes to be committed:
    	new file:   script_migration_password.php

---
+++ script_migration_password.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . '/config/db.php';
+
+$pdo=getDB();
+
+$pdo->beginTransaction();
+//Récupérer tout les mots de passe actuels
+$stmt=$pdo->prepare("SELECT numro_mobile_money as numero,mot_passe FROM participants");
+$stmt->execute();
+$resultats=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$nombreMigration=0;
+foreach($resultats as $resultat){
+    $password_nonHasher=$resultat['mot_passe'];
+    $passwordHasher=password_hash($password_nonHasher,PASSWORD_DEFAULT);
+
+    //Mettre à jour maintenant le mot de passe
+    $stmt2=$pdo->prepare("UPDATE participants set mot_passe=? WHERE numro_mobile_money=?");
+    $stmt2->execute([$passwordHasher,$resultat['numero']]);
+    
+    if($stmt->rowCount()>0){
+        $nombreMigration++;
+    }
+}
+$pdo->commit();
+if($nombreMigration>0){
+    echo 'Migration éffectuée avec succès !';
+}else{
+    echo 'Une erreur s\'est produite dans le script';
+}
+
+?>
